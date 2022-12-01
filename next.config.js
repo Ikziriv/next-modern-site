3231b7d02602d5755c71735404d256290fa2b75a
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2022-12-01 22:35:25 +0700

    First commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: false,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
